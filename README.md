82005c74790a8dc250a3803d07ffe9bdd788e6d1
Elangoraja <se.elangoraja10@gmail.com>
2023-01-30 22:56:55 +0530

    pusing spring
    Spring description

--- README.md
+++
@@ -1,8 +0,0 @@
-# Testing
-This is for Testing and Learning purpose
-Adding the Description
-
-Adding data from the local
-
-adding the second line of data from local
-
---
+++ src/test/java/io/elango/demoTesting/DemoTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package io.elango.demoTesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
